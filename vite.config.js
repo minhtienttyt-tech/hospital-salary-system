84ac53ed237f11b90246f076230e4fa1ccc16cab
minhtienttyt-tech <minhtienttyt@gmail.com>
2026-08-10 09:23:15 +0700

    9:23 10/8

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
-  base: '/hospital-salary-system/',
+  plugins: [viteSingleFile()],
+  base: './',
   build: {
     outDir: 'dist',
   }
